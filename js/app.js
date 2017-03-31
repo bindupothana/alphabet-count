8d726d620284b6100d12a107ebc6b4d5b8c23351
bindupothana <himabindu.pothana@navaratan.com>
2017-03-31 18:44:24 +0530

    Vowel and consonant count

--- js/app.js
+++ js/app.js
@@ -2,19 +2,26 @@ var app=angular.module('charApp',[]);
 app.controller('charCtrl',function($scope){
  $scope.charCount=function(string){
       $scope.obj={};
-      $scope.vowelsCount = 0;
-       var consonant = ' bcdfghjklmnpqrstvwxyz';
+    $scope.vowelsCount = 0;
+       $scope.consonants = 0;
         var length = string.length;
         var strl = string.charAt(i)
 
 
       
-      for(var i = 1; i < length-1; i++) {
+      for(var i = 0; i < length; i++) {
         console.log("gggg",string.length)
+          var strl = string.charAt(i)
          if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
-              $scope.vowelsCount += 1;
-            }
-                console.log("vowel",$scope.vowelsCount )
+              $scope.vowelsCount = $scope.vowelsCount + 1;
+
+            console.log("vowel",$scope.vowelsCount )
+          }
+          else if((string.charAt(i).match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/))){
+
+           $scope.consonants++;
+           console.log ("consonants: " , $scope.consonants)
+           }
 
           }
         
